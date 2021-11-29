ccdbdc68817e7ff8968cb70d25bf938f05455fa1
Ben Strathdee <ben.strathdee@live.com.au>
2021-11-29 15:55:16 +1100

    refactored with Remi

--- javascript/modules/translate.js
+++ javascript/modules/translate.js
@@ -1,54 +1,43 @@
-import { alphaToMorse, morseToAlpha } from "./translation-keys.js";
+// import { alphaToMorse, morseToAlpha } from "./translation-keys.js";
 
-export const translate = (translator, inputFrom) => {
+export const translate = (mode, object, text) => {
     const error = "Invalid characters present, please try again.";
-    let toTranslate = document.getElementById(inputFrom).value;
-    let translateArr = [];
-    let translatedArr = [];
 
-    if (translator === alphaToMorse) {
-        translateArr = toTranslate.toLowerCase().split('');
-        const entriesArray = Object.values(morseToAlpha);
-        const everyCheck = (item) => entriesArray.includes(item);
+    let charArr = [];
 
-        if (translateArr.every((item) => everyCheck(item))) {
-            translatedArr = translateArr.map((char) => {
-                if (char === " ") {
-                    return " / "
-                }
-                for (const [key, value] of Object.entries(alphaToMorse)) {
-                    if (key === char) {
-                        return value + " ";
-                    };
-                }
-            })
+    const everyCheck = (item) => Object.keys(object).includes(item);
 
+    if (mode === "alphaToMorse") {
+        charArr = text.toLowerCase().split("");
+
+        if (charArr.every((char) => everyCheck(char))) {
+            return charArr
+                .map((char) => {
+                    for (const [key, value] of Object.entries(object)) {
+                        if (key === char) {
+                            return value + " ";
+                        }
+                    }
+                })
+                .join("");
         } else {
             return error;
         }
-        
     } else {
-
-        translateArr = toTranslate.split(' ');
-        const checkArr = toTranslate.split('').filter((item) => item != "/");
-        const entriesArray = Object.values(alphaToMorse);
-        const everyCheck = (item) => entriesArray.includes(item);
-
-        if (checkArr.every((item) => everyCheck(item))) {
-            translatedArr = translateArr.map((char) => {
-                if (char === "/") {
-                    return " "
-                }
-                for (const [key, value] of Object.entries(morseToAlpha)) {
-                    if (key === char) {
-                        return value;
-                    };
-                }
-            })
+        charArr = text.split(" ");
+        
+        if (charArr.every((item) => everyCheck(item))) {
+            return charArr
+                .map((char) => {
+                    for (const [key, value] of Object.entries(object)) {
+                        if (key === char) {
+                            return value;
+                        }
+                    }
+                })
+                .join("");
         } else {
             return error;
         }
-        
     }
-    return translatedArr.join('');
-}
+};
--- javascript/script.js
+++ javascript/script.js
@@ -3,11 +3,12 @@ import { translate } from "./modules/translate.js";
 
 const morseButton = document.getElementById("translateToMorse");
 const alphaButton = document.getElementById("translateToAlpha");
-const result = document.getElementById("result")
+const input = document.getElementById("input");
+const result = document.getElementById("result");
 
 morseButton.addEventListener("click", () => {
-    result.innerText = translate(alphaToMorse, "input");
-})
+    result.innerText = translate("alphaToMorse", alphaToMorse, input.value);
+});
 alphaButton.addEventListener("click", () => {
-    result.innerText = translate(morseToAlpha, "input")
-})
+    result.innerText = translate("morseToAlpha", morseToAlpha, input.value);
+});
